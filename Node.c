675eb478cd6ec91e7e7fc952c6fd8303ea573355
Copperfield <xwhfcenter@gmail.com>
2014-11-24 17:33:27 +0800

    添加链表学习的相关源代码

---
+++ Node.c
@@ -0,0 +1,17 @@
+#include <List.h>
+
+struct Node {
+  ElementType Element;
+  Position Next;
+};
+
+/* Return true if L is empty */
+int IsEmpty(List L) {
+  return L -> next == NULL;
+}
+
+/* Return true if P is the last position in list L */
+/* Parameter L is unused in this implementation */
+int IsLast(Position P, List L) {
+  return P -> next == NULL;
+}

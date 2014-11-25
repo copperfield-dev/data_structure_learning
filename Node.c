00ef1b6a39a6174e88e555093d398bf622b8ba17
Copperfield <xwhfcenter@gmail.com>
2014-11-25 11:31:37 +0800

    增加Find, FindPrevious, Delete, Insert例程

--- Node.c
+++ Node.c
@@ -2,7 +2,7 @@
 
 struct Node {
   ElementType Element;
-  Position Next;
+  Position next;
 };
 
 /* Return true if L is empty */
@@ -14,4 +14,57 @@ int IsEmpty(List L) {
 /* Parameter L is unused in this implementation */
 int IsLast(Position P, List L) {
   return P->next == NULL;
+}
+
+/* Return Position of X in L; NULL if not found */
+Position Find(ElementType X, List L) {
+  Position P;
+
+  P = L->Next;
+  while(P != NULL && P->Element != X)
+    P = P->next;
+
+  return P;
+}
+
+/* Delete first occurrence of X from a list */
+/* Assume use of a header node */
+void Delete(ElementType X, List L) {
+  Position P, tmpCell;
+
+  P = FindPrevious(X, L);
+
+  if (!IsLast(P, L)) {           // Assumption of header use
+    tmpCell = P->next;           // X is found; delete it
+    P->next = tmpCell->next;     // Bypass deleted cell
+    free(tmpCell);
+  }
+}
+
+/* If X is not found, then next field of returned */
+/* Position is NULL*/
+/* Assumes a header */
+Position FindPrevious(ElementType X, List L) {
+  Position P;
+
+  P = L;
+  while(P->next != NULL && P->next->Element != X)
+    P = P->next;
+
+  return P;
+}
+
+/* Insert (after legal position P */
+/* Header implementation assumed */
+/* Parameter L is unused in this implementation */
+void Insert(ElementType X, List L, Position P) {
+  Position tmpCell;
+
+  tmpCell = malloc(sizeof(struct Node));
+  if (tmpCell == NULL)
+    FatalError("Out of space!!!");
+
+  tmpCell->Element = X;
+  tmpCell->next = P->next;
+  P->next = tmpCell;
 }

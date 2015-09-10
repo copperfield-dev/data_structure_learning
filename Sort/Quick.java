9b6aebdbc6602de5fe785ab72ef46872b75b6149
xwhfcenter@gmail.com <xwhfcenter@gmail.com>
2015-09-10 23:12:20 +0800

    提交了快速排序的partition()函数

--- Sort/Quick.java
+++ Sort/Quick.java
@@ -10,4 +10,23 @@ public class Quick {
         sort(a, lo, j - 1);
         sort(a, j + 1, hi);
     }
+
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo, j = hi + 1;
+        Comparable v = a[lo];
+        while (true) {
+            while (less(a[++i], v)) {
+                if (i == hi)
+                    break;
+            }
+            while (less(v, a[--j])) {
+                if (j == lo)
+                    break;
+            }
+            if (i >= j) break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
 } 
